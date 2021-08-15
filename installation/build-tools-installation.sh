880b4df08b871c055f7b34833a127022b44b0b27
Lewis Chan <yeouw95@gmail.com>
2021-08-15 16:16:16 +0800

    Update installation for zsh

--- installation/build-tools-installation.sh
+++ installation/build-tools-installation.sh
@@ -2,9 +2,9 @@
 
 # install build essentials
 sudo apt update
-sudo apt install build-essential
+sudo apt install build-essential -y
 
 # install git from ppa
 sudo apt-add-repository ppa:git-core/ppa
 sudo apt update
-sudo apt install git
+sudo apt install git -y
--- installation/zsh-installation.sh
+++ installation/zsh-installation.sh
@@ -4,11 +4,11 @@
 sudo apt install -y zsh
 
 # install Oh My Zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sh -c '$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)'
 
 # install custom theme
-git clone https://github.com/geometry-zsh/geometry $HOME/.oh-my-zsh/custom/themes/geometry
+git clone https://github.com/geometry-zsh/geometry '$HOME/.oh-my-zsh/custom/themes/geometry'
 
 # install custom plugins
-git clone https://github.com/zsh-users/zsh-autosuggestions.git $HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions.git '$HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions'
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git '$HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting'
