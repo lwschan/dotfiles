a7a936832d99bbcd4dd30e2d12c533060af5e959
Lewis Chan <yeouw95@gmail.com>
2021-08-15 16:38:30 +0800

    Update installation

--- install-extra.sh
+++ install-extra.sh
@@ -2,12 +2,6 @@
 
 chmod +x ./installation/*.sh
 
-echo 'Installing build essentials...'
-./installation/build-tools-installation.sh
-
-echo 'Installing zsh...'
-./installation/zsh-installation.sh
-
 echo 'Installing zsh plugins...'
 ./installation/omz-custom-installation.sh
 
