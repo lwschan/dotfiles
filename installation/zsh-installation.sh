1b466ec8305fff1c8aef64ed654b8dbc4ac4f292
Lewis Chan <yeouw95@gmail.com>
2021-08-15 12:56:05 +0800

    Update zsh-installation

--- installation/zsh-installation.sh
+++ installation/zsh-installation.sh
@@ -7,8 +7,8 @@ sudo apt install -y zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # install custom theme
-git clone https://github.com/geometry-zsh/geometry $ZSH_CUSTOM/themes/geometry
+git clone https://github.com/geometry-zsh/geometry $HOME/.oh-my-zsh/custom/themes/geometry
 
 # install custom plugins
-git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions.git $HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
