09a90f7d302e340c529db53e0f07c162886016b2
Lewis Chan <lch@timelog.com>
2022-04-07 10:29:43 +0800

    update dotfiles to install cmake

--- installation/build-tools-installation.sh
+++ installation/build-tools-installation.sh
@@ -3,6 +3,7 @@
 # install build essentials
 sudo apt update
 sudo apt install build-essential -y
+sudo apt install cmake
 
 # install essentials for WSLg
 sudo apt install libgtk2.0-0 libgtk-3-0 libgbm-dev libnotify-dev libgconf-2-4 libnss3 libxss1 libasound2 libxtst6 xauth xvfb
