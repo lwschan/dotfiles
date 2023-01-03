9b2f5933a14fd1eb9852bfcda842fc2e610e4f10
Lewis Chan <8966669+lwschan@users.noreply.github.com>
2023-01-03 14:04:36 +0800

    Add traceroute installation

--- install-scripts/dev.sh
+++ install-scripts/dev.sh
@@ -16,8 +16,11 @@ sudo apt -y install git
 sudo apt -y install keychain
 
 # install zip and unzip packages
-sudo apt-get install zip unzip
+sudo apt -y install zip unzip
 
 # install yq
 sudo wget https://github.com/mikefarah/yq/releases/latest/download/yq_linux_amd64 -O /usr/bin/yq &&\
     sudo chmod +x /usr/bin/yq
+
+# install traceroute
+sudo apt -y install traceroute
